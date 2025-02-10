124c5aa33639bf5acff9fba572f58b28eededa85
Aomedwalid <ahmedwalid9876543@gmail.com>
2025-02-10 03:04:37 +0200

    mini message board

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const indexRoute = require('./routes/routesMessages');
+
+//setting the pathes
+
+const assetsPath = path.join(__dirname, 'public');
+
+//middlewares and the static 
+
+app.use(express.static(assetsPath));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//now let's set up the ejs engine
+
+app.set('view engine' , 'ejs');
+
+//get requiestes
+
+app.use('/',indexRoute);
+//listener
+
+app.listen(3000);
